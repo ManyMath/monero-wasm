ef9d5ec6229bf7e1530f521c4865baabf5396d9d
sneurlax <sneurlax@gmail.com>
2024-10-12 19:51:16 -0500

    tokio blocks wasm-unknown-unknown & ∴ wasm-pack & wasm-bindgen
    web takeaway for the day: tokio doesn't support wasm32-unknown-unknown. will have to use wasm32-unknown-wasi, which tokio supports. i don't think wasm32-unknown-emscripten is going to help
    so: skip the wasm-pack and wasm-bindgen crates, which use wasm32-unknown-unknown
    see https://github.com/rustwasm/wasm-bindgen/issues/3421
    probably no big deal and other tools will work, this was just the first resource I tried

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+use monero_rust::{MoneroWallet, Language, Network};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -9,5 +10,23 @@ extern "C" {
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, monero-wasm!");
+    // Generate a mnemonic seed in English
+    let mnemonic = MoneroWallet::generate_mnemonic(Language::English);
+    println!("Generated mnemonic: {}", mnemonic);
+
+    let mut primary_address = String::new();
+    // Create a wallet from the mnemonic
+    match MoneroWallet::new(&mnemonic, Network::Mainnet) {
+        Ok(wallet) => {
+            // Get the primary address of the wallet
+            primary_address = wallet.get_primary_address();
+            println!("Primary address: {}", primary_address);
+        }
+        Err(e) => {
+            eprintln!("Failed to create wallet: {}", e);
+        }
+    }
+
+    let message = format!("{}: {}", &mnemonic, &primary_address);
+    alert(message.as_str());
 }
